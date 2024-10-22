4f91e06243bf59a45ba291403786a35535f662ce
ghej <ghej@itu.dk>
2024-10-22 11:06:15 +0200

    wrote boilerplate code and function statements.

--- Server/server.go
+++ Server/server.go
@@ -1,10 +1,48 @@
 package main
 
 import (
+	"context"
 	proto "example/chittychat/grpc"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
 )
 
 type ChittyChatServer struct {
 	proto.UnimplementedChittyChatServiceServer
 	messageLog []string
+}
+
+func (s *ChittyChatServer) PostMessage(ctx context.Context, in *proto.Message) (*proto.Confirm, error) {
+
+}
+
+func (s *ChittyChatServer) JoinMessageBoard(ctx context.Context, in *proto.Confirm) (grpc.ServerStreamingClient[Message], error) {
+
+}
+
+func main() {
+	server := ChittyChatServer{
+		messageLog: make([]string, 0),
+	}
+	go server.start()
+
+	wait := make(chan bool)
+	<-wait
+}
+
+func (s *ChittyChatServer) start() {
+	listener, err := net.Listen("tcp", "localhost:5050")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	proto.RegisterChittyChatServiceServer(grpcServer, s)
+	fmt.Printf("server listening at %v", listener.Addr())
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
